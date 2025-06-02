05bec588c38442f744886e123201bc0f72d20116
Aiden B <156241225+791747hnlhymt@users.noreply.github.com>
2025-06-02 14:57:12 -0400

    Create dataScience.py

---
+++ dataScience.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+df = pd.read_csv(r"")
+pd.set_option("display.max_columns" , None)
+
+
+print("The Robots data")
+print("")
+print("")
+print("Based off your input here is the statistics of this robot")
+print("")
+print("")
+
+filterd_table = df[[]]
+#adding data science prototype
